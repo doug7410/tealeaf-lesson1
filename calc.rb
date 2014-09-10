75a96c1a645c4511f74babe50f8319bfbcf383de
mary-dean <mary-dean@rcn.com>
2014-09-10 15:09:20 -0400

    Cleaned up calc.rb code

--- calc.rb
+++ calc.rb
@@ -1,4 +1,3 @@
-require 'pry'
 
 intro = <<END_MARKER
 
@@ -25,21 +24,22 @@ puts "1. Add          4. Division     7. Gamma
 3. Multiply     6. Log, base    9. Factorial"
 puts "\n"
 z = gets.chomp[0].to_i
-choice = case z
-when 1 then "add"
-when 2 then "subtract"
-when 3 then "multiply"
-when 4 then "divide"
-when 5 then "calculate the hypotenuse"
-when 6 then "log"
-when 7 then "do whatever gamma does"
-when 8 then "inverse hyperbolic tangent"
-when 9 then "calculate the factorial"
-else "\n...That is not a valid choice for "
-end
 
-  puts "\nYou have chosen to #{choice} #{x} and #{y}"
-  print "\nDrum roll, please. . ."
+choice = case z
+  when 1 then "add"
+  when 2 then "subtract"
+  when 3 then "multiply"
+  when 4 then "divide"
+  when 5 then "calculate the hypotenuse"
+  when 6 then "log"
+  when 7 then "do whatever gamma does"
+  when 8 then "inverse hyperbolic tangent"
+  when 9 then "calculate the factorial"
+  else "\n...That is not a valid choice for "
+  end
+
+puts "\nYou have chosen to #{choice} #{x} and #{y}"
+print "\nDrum roll, please. . ."
 show_text = "pah RUM pum pum PUM"
 c = 0
 show_text.length.times do
@@ -47,25 +47,26 @@ show_text.length.times do
   c += 1
   sleep 0.15
 end
-  begin
 
+begin  #start of error handling
 answer = case z
-when 1 then x + y
-when 2 then x - y
-when 3 then x * y
-when 4 then x / y
-when 5 then Math.hypot(x,y)
-when 6 then log(x,y)
-when 7 then [Math.gamma(x), Math.gamma(y)]
-when 8 then [Math.atanh(x), Math.atanh(y)]
-when 9 then [(1..x).inject(1, :*), (1..y).inject(1, :*)]
-else 0
-end
+  when 1 then x + y
+  when 2 then x - y
+  when 3 then x * y
+  when 4 then x / y
+  when 5 then Math.hypot(x,y)
+  when 6 then log(x,y)
+  when 7 then [Math.gamma(x), Math.gamma(y)]
+  when 8 then [Math.atanh(x), Math.atanh(y)]
+  when 9 then [(1..x).inject(1, :*), (1..y).inject(1, :*)]
+  else 0
+  end
 rescue 
   puts "\n\n        I'm sorry, but your answer lies beyond the 
-       frontiers of current mathematical understanding"
-           exit
-  end
+      frontiers of current mathematical understanding"
+  exit
+end  #end of error handling
+
 puts "\n\n            Ladies and gentlemen, the answer is 
                 ===>    #{answer}    <==="
                                
